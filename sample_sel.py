72342a81dce9b0ae4b2573c194ecbc9b4a5254ae
kiran1724 <kiran.uppuluri17@gmail.com>
2020-07-22 16:08:13 +0530

    updated driver to remote

--- sample_sel.py
+++ sample_sel.py
@@ -1,7 +1,12 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome('/usr/bin/chromedriver')
-driver.get('http://stackoverflow.com')
+desiredCapabilities={
+"browserName":"chrome"
+}
+
+driver = webdriver.Remote(desired_capabilities=desiredCapabilities, 
+			command_executor='http://10.0.2.15:4444/wd/hub')
+driver.get('https://www.amazon.in/')
 print("Title: "+ str(driver.title))
 print("Closing driver")
 driver.quit()
